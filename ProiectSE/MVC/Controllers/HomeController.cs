6d59560f5ed5ee30efdff791a0772419fde3ac75
andreeamateiasi <andreea.mateiasi@yahoo.com>
2017-04-24 16:46:08 +0300

    Views for: apartment, home, bill, remaining debts, contracts, my profile, contact

---
+++ ProiectSE/MVC/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    //[Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult MyProfile()
+        {
+            return View();
+        }
+        public ActionResult Apartment()
+        {
+            return View();
+        }
+        public ActionResult Bill()
+        {
+            return View();
+        }
+        public ActionResult WaterConsumption()
+        {
+            return View();
+        }
+        public ActionResult RemainingDebts()
+        {
+            return View();
+        }
+        public ActionResult Payments()
+        {
+            return View();
+        }
+        public ActionResult Contracts()
+        {
+            return View();
+        }
+        public ActionResult Employees()
+        {
+            return View();
+        }
+    }
+}
