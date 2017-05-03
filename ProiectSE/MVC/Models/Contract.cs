5b6f372cee5de12aded0b1b6a68ad1268e3dc0cc
IuliaSimion <iulia_20ian@yahoo.com>
2017-05-03 19:15:27 +0300

    MVC models added for apartment, contract, remaining debt

---
+++ ProiectSE/MVC/Models/Contract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Contract
+    {
+        public int ContractId { get; set; }
+        public string Supplier { get; set; }
+        public string ContractPeriod { get; set; }
+        public string ServicesFacilitiesOffered { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
---
+++ ProiectSE/MVC/Models/RemainingDebt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class RemainingDebt
+    {
+        public int RemainingDebtId { get; set; }
+        public int ApartmentId { get; set; }
+        public string DebtTo { get; set; }
+        public string Month { get; set; }
+        public decimal AmountOfMoneyOwed { get; set; }
+        public virtual Apartment Apartment { get; set; }
+    }
+}
