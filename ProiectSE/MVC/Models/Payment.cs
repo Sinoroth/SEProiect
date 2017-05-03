77278ec44205e72ad1724ebda4b9ce72d883a2ff
andraserban <andra.serban2095@gmail.com>
2017-05-03 20:51:06 +0300

    -user,payment model(MVC)

---
+++ ProiectSE/MVC/Models/Payment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public int ApartmentId { get; set; }
+        public string ServicesToBePaid { get; set; }
+        public decimal AmountOfMoneyToBePaid { get; set; }
+        public string Paid { get; set; }
+        public virtual Apartment Apartment { get; set; }
+    }
+}
