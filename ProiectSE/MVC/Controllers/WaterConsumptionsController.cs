0211516563f04df4ad3f8cda8da731c0e5d43429
AncutaSimion <ancuta.simion06@gmail.com>
2017-05-08 22:12:03 +0300

    WaterCosumption MVC Controller&Views

---
+++ ProiectSE/MVC/Controllers/WaterConsumptionsController.cs
@@ -0,0 +1,27 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class WaterConsumptionsController : Controller
+    {
+        // GET: WaterConsumptions
+        public ActionResult List()
+        {
+            WaterConsumption wCons = new WaterConsumption();
+            wCons.WaterConsumptionId = 1;
+            wCons.ApartmentId = 1;
+            wCons.PricePerUnit = 20;
+            wCons.Consumption = 10;
+            wCons.AmountOfMoneyOwed = 200;
+            List<WaterConsumption> wConsList = new List<WaterConsumption>();
+            wConsList.Add(wCons);
+
+            return View(wConsList);
+        }
+    }
+}
